33d0bcd1ac42a428e91660a59534a4e760b4bd3f
lsxiao <faiht.epiphone@qq.com>
2017-04-23 12:28:19 +0800

    Utils Unit Testcase

--- processor/src/main/java/com/lsxiao/apllo/processor/Utils.kt
+++ processor/src/main/java/com/lsxiao/apllo/processor/Utils.kt
@@ -1,16 +1,7 @@
 package com.lsxiao.apllo.processor
 
 object Utils {
-    fun arraySplitBy(list: List<String>, separator: String): String {
-        var temp = ""
-        list.forEach { s ->
-            when {
-                list.indexOf(s) + 1 != list.size -> temp += wrapBy(s, "\"") + separator
-                else -> temp += wrapBy(s, "\"")
-            }
-        }
-        return temp
+    fun split(list: List<String>, separator: String): String {
+        return list.mapTo(ArrayList<String>()) { "\"$it\"" }.joinToString(separator)
     }
-
-    fun wrapBy(s: String, wrapper: String): String = wrapper + s + wrapper
 }
