bbbc0b65025dfa31ae2b58733d8fe14e87883747
lsxiao <faith.epiphone@qq.com>
2017-05-17 21:01:03 +0800

    use kryo to serialize object.

---
+++ core/src/main/java/com/apollo/core/serialize/Serializable.kt
@@ -0,0 +1,15 @@
+package com.apollo.core.serialize
+
+/**
+ * write with Apollo
+ * author:lsxiao
+ * date:2017-05-17 17:23
+ * github:https://github.com/lsxiao
+ * zhihu:https://zhihu.com/people/lsxiao
+ */
+
+interface Serializable {
+    fun serialize(obj: Any): ByteArray
+
+    fun <T> deserialize(data: ByteArray, clazz: Class<T>): T
+}
